c8e69249b89f458dd30bd435a057f8ef76390f84
Jeong Mingyo <alsry922@gmail.com>
2026-05-06 15:57:10 +0900

    [Passed] 점 개수 세기 3 (269ms, 29MB)

--- trail5/점 개수 세기 3/count-number-of-points-3.py
+++ trail5/점 개수 세기 3/count-number-of-points-3.py
@@ -1,26 +1,25 @@
 from sortedcontainers import SortedSet
-
-n, q = map(int, input().split()) # 점 개수, 질의 수
-points = SortedSet(list(map(int, input().split())))
+n, q = map(int, input().split())
+points = list(map(int, input().split()))
 queries = [tuple(map(int, input().split())) for _ in range(q)]
 
 # Please write your code here.
-map = {}
+nums = SortedSet(points)
 cnt = 1
-for point in points:
-    map[point] = cnt
+mapper = {}
+for num in nums:
+    mapper[num] = cnt
     cnt += 1
 
-comp_points = [0] + [1 for _ in range(len(points))]
+prefix_sum = [0 for _ in range(n + 1)]
+for point in points:
+    idx = mapper[point]
+    prefix_sum[idx] += 1
 
-prefix_sum = [0 for _ in range(len(points) + 1)]
-for i in range(1, len(comp_points)):
-    prefix_sum[i] = prefix_sum[i - 1] + comp_points[i]
+for i in range(1, n + 1):
+    prefix_sum[i] += prefix_sum[i - 1]
 
-out = []
 for query in queries:
-    # 질의의 나오는 좌표를 압축
-    start, end = map[query[0]], map[query[1]]
-    out.append(str(prefix_sum[end] - prefix_sum[start - 1]))
-
-print('\n'.join(out))
+    x1, x2 = query
+    lx, rx = mapper[x1], mapper[x2]
+    print(prefix_sum[rx] - prefix_sum[lx - 1])
