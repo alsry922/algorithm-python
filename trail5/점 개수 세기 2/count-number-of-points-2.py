0d6d4d666ca5f5d256191dc49f910142f08d92ba
Jeong Mingyo <alsry922@gmail.com>
2026-05-11 15:39:19 +0900

    [Passed] 점 개수 세기 2 (1479ms, 139MB)

--- trail5/점 개수 세기 2/count-number-of-points-2.py
+++ trail5/점 개수 세기 2/count-number-of-points-2.py
@@ -7,43 +7,33 @@ queries = [tuple(map(int, input().split())) for _ in range(q)]
 def print_grid(grid):
     for row in grid:
         print(row)
-    print('==========')
-# Please write your code here.
+    print()
+
 xs = SortedSet()
 ys = SortedSet()
-for x, y in points:
-    xs.add(x)
-    ys.add(y)
-
-
-grid = [
-    [0 for _ in range(len(ys))]
-    for _ in range(len(xs))
-]
-
-for x, y in points:
-    xi = xs.bisect_left(x)
-    yi = ys.bisect_left(y)
-    grid[xi][yi] += 1
-
-prefix_sum = [
+psum = [
     [0 for _ in range(n + 1)]
     for _ in range(n + 1)
 ]
+for point in points:
+    x, y = point
+    xs.add(x)
+    ys.add(y)
 
+for x, y in points:
+    xp = xs.bisect_left(x) + 1
+    yp = ys.bisect_left(y) + 1    
+    psum[xp][yp] = 1
+
+# Please write your code here.
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        prefix_sum[i][j] = prefix_sum[i][j-1] + \
-                            prefix_sum[i-1][j] - \
-                            prefix_sum[i-1][j-1] + \
-                            grid[i-1][j-1]
+        psum[i][j] += (psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1])
 
 for query in queries:
     x1, y1, x2, y2 = query
-    xi1 = xs.bisect_left(x1) + 1
-    yi1 = ys.bisect_left(y1) + 1
-    xi2 = xs.bisect_right(x2)
-    yi2 = ys.bisect_right(y2)
-
-    print(prefix_sum[xi2][yi2] - prefix_sum[xi1 - 1][yi2] - prefix_sum[xi2][yi1 - 1] + \
-        prefix_sum[xi1 - 1][yi1 - 1])
+    x1i = xs.bisect_left(x1) + 1
+    y1i = ys.bisect_left(y1) + 1
+    x2i = xs.bisect_right(x2)
+    y2i = ys.bisect_right(y2)
+    print(psum[x2i][y2i] - psum[x2i][y1i - 1] - psum[x1i - 1][y2i] + psum[x1i - 1][y1i - 1])
