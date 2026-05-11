6021426ef3ddc40cd9920d21a25eee98a9ef6a6c
Jeong Mingyo <alsry922@gmail.com>
2026-05-11 09:58:11 +0900

    [Passed] 점 개수 세기 (331ms, 45MB)

--- trail5/점 개수 세기/count-number-of-points.py
+++ trail5/점 개수 세기/count-number-of-points.py
@@ -1,35 +1,28 @@
 from sortedcontainers import SortedSet
+import sys
+input = sys.stdin.readline
+n, q = map(int, input().split())
+points = SortedSet(list(map(int, input().split())))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
 
-# 변수 선언 및 입력:
-n, q = tuple(map(int, input().split()))
-arr = list(map(int, input().split()))
-queries = [
-    tuple(map(int, input().split()))
-    for _ in range(q)
-]
+# Please write your code here.
+# mapper = {}
+# cnt = 1
+# for point in points:
+#     mapper[point] = cnt
+#     cnt += 1
 
-mapper = {}
+# prefix_sum = [0 for _ in range(n + 1)]
+# for point in points:
+#     prefix_sum[mapper[point]] += 1
 
-# 주어진 x좌표값들을 전부 treeset에 넣어줍니다.
-nums = SortedSet(arr)
+# for i in range(1, n + 1):
+#     prefix_sum[i] = prefix_sum[i - 1] + 1
 
-# 예외를 처리하기 위해 최댓값보다 큰 값을 treeset에 넣어줍니다.
-nums.add((10 ** 9) + 1)
-
-# treeset에서 정점을 작은 번호부터 뽑으면서
-# 각 정점별로 1번부터 순서대로 매칭하여
-# 그 결과를 hashmap에 넣어줍니다.
-cnt = 1
-for num in nums:
-    mapper[num] = cnt
-    cnt += 1
-
-# 질의에 대해
-# 각 [a, b]에 해당하는 번호를
-# mapper를 통해 구해
-# 두 번호 사이의 점의 수를 출력합니다.
-for a, b in queries:
-    new_a = mapper[nums[nums.bisect_left(a)]]
-    new_b = mapper[nums[nums.bisect_right(b)]]
-
-    print(new_b - new_a)
+out = []
+for query in queries:
+    l, r = query
+    li = points.bisect_left(l)
+    ri = points.bisect_right(r)
+    out.append(str(ri - li))
+print('\n'.join(out))
