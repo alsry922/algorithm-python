6bac8bbb79d73ac6dc2470ae702705c5da56c564
Jeong Mingyo <alsry922@gmail.com>
2026-05-13 14:14:50 +0900

    [Passed] 인접하지 않은 3개의 숫자 (69ms, 24MB)

--- trail5/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.py
+++ trail5/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.py
@@ -2,17 +2,20 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-L = [arr[0] for _ in range(n)]
-R = [arr[-1] for _ in range(n)]
+pmax = [0] * (n) # 0 ~ i - 1 까지 최댓값
+smax = [0] * (n) # i ~ n - 1 까지 최댓값
 
+pmax[0] = arr[0]
 for i in range(1, n):
-    L[i] = max(L[i - 1], arr[i])
+    pmax[i] = max(pmax[i - 1], arr[i])
 
-for j in range(n - 2, -1, -1):
-    R[j] = max(R[j + 1], arr[j])
+smax[-1] = arr[-1]
+for i in range(n - 2, -1, -1):
+    smax[i] = max(smax[i + 1], arr[i])
 
 answer = 0
-for i in range(2, n - 2):
-    answer = max(answer, arr[i] + L[i - 2] + R[i + 2])
+for i in range(n):
+    if 2 <= i <= n - 3:
+        answer = max(answer, arr[i] + pmax[i - 2] + smax[i + 2])
 
 print(answer)
