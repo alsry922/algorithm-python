9c598c05336176fa9658dcca51f450e91b0755c7
Jeong Mingyo <alsry922@gmail.com>
2026-05-13 13:34:44 +0900

    [Passed] 겹치지 않는 선분 (217ms, 35MB)

--- trail5/겹치지 않는 선분/line-segments-that-do-not-overlap.py
+++ trail5/겹치지 않는 선분/line-segments-that-do-not-overlap.py
@@ -2,31 +2,20 @@ n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 lines = sorted(lines, key=lambda x: x[0])
-pmax = [0] * n
-smin = [0] * n
-pmax[0] = lines[0][1]
-for i in range(1, n):
-    if lines[i][1] > pmax[i - 1]:
-        pmax[i] = lines[i][1]
-    else:
-        pmax[i] = pmax[i - 1]
-smin[-1] = lines[-1][1]
-for i in range(n - 2, -1, -1):
-    if lines[i][1] < smin[i + 1]:
-        smin[i] = lines[i][1]
-    else:
-        smin[i] = smin[i + 1]
+pmax = [-float('inf')] + [0] * n  # pmax[0]이 센티넬
+smin = [0] * n + [float('inf')]   # smin[n]이 센티넬
+for i in range(1, n + 1):
+    pmax[i] = max(pmax[i - 1], lines[i - 1][1])  # 1부터 시작
+
+for i in range(n - 1, -1, -1):
+    smin[i] = min(smin[i + 1], lines[i][1])
+    
 answer = 0
 for i in range(n):
-    cross = True
     x1, x2 = lines[i]
-    if i == 0 and smin[i + 1] > x2: 
-        cross = False
-    elif i == n - 1 and pmax[i - 1] < x2:
-        cross = False
-    else:
-        cross = not (pmax[i - 1] < x2 and smin[i + 1] > x2)
+    cross = not (pmax[i] < x2 and x2 < smin[i + 1])
     if not cross:
         answer += 1
+
 
 print(answer)
