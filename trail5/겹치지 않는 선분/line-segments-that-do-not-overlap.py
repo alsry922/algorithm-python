a7508b7ceb0d1c28170f51fd96b9933880e37dd1
Jeong Mingyo <alsry922@gmail.com>
2026-05-14 13:44:05 +0900

    [Passed] 겹치지 않는 선분 (208ms, 29MB)

--- trail5/겹치지 않는 선분/line-segments-that-do-not-overlap.py
+++ trail5/겹치지 않는 선분/line-segments-that-do-not-overlap.py
@@ -1,21 +1,20 @@
 n = int(input())
-lines = [tuple(map(int, input().split())) for _ in range(n)]
+lines = sorted([tuple(map(int, input().split())) for _ in range(n)])
+
 # Please write your code here.
-lines = sorted(lines, key=lambda x: x[0])
-pmax = [-float('inf')] + [0] * n  # pmax[0]이 센티넬
-smin = [0] * n + [float('inf')]   # smin[n]이 센티넬
+MIN = -1000000
+MAX = 1000000
+pmax = [MIN] * (n + 1) # 0 ~ i-1 까지의 최대
+smin = [MAX] * (n + 1) # i ~ n-1 까지의 최소
+
 for i in range(1, n + 1):
-    pmax[i] = max(pmax[i - 1], lines[i - 1][1])  # 1부터 시작
+    pmax[i] = max(pmax[i - 1], lines[i - 1][1])
 
 for i in range(n - 1, -1, -1):
     smin[i] = min(smin[i + 1], lines[i][1])
-    
-answer = 0
+
+cnt = 0
 for i in range(n):
-    x1, x2 = lines[i]
-    cross = not (pmax[i] < x2 and x2 < smin[i + 1])
-    if not cross:
-        answer += 1
-
-
-print(answer)
+    x2 = lines[i][1]
+    cnt += 1 if pmax[i] < x2 < smin[i + 1] else 0
+print(cnt)
