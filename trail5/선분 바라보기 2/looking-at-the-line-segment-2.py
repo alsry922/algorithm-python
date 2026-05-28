5b1a5eaf4a6cec2953ae4cb723cf97e1fd51a7fc
Jeong Mingyo <alsry922@gmail.com>
2026-05-28 17:32:38 +0900

    [Passed] 선분 바라보기 2 (443ms, 26MB)

--- trail5/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ trail5/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -1,33 +1,37 @@
-import heapq
+from sortedcontainers import SortedSet
 n = int(input())
 
-points = []
-for i in range(n):
-    yi, x1i, x2i = map(int, input().split())
-    points.append(( x1i, +1, i, yi ))
-    points.append(( x2i, -1, i, yi ))
-
+segments = [ tuple(map(int, input().split())) for _ in range(n) ]
 # Please write your code here.
-points.sort()
-segs = []
-deleted = set()
-visible = [False] * n
+segments.sort()
+covered = SortedSet()
 cnt = 0
-for x, v, index, y in points:
-    # 시작점이면 추가
-    if v == 1:
-        heapq.heappush(segs, (y, index))
-    else:
-        deleted.add((y, index))
+for y, x1, x2 in segments:
+    checkIdx = covered.bisect_right(( x1, x2 )) - 1
+    # 나보다 앞에 있는 선분이 있고, 앞 선분과 완전히 겹치는 경우는 아무것도 안함.
+    if checkIdx >= 0:
+        cx1, cx2 = covered[checkIdx]
+        if cx1 <= x1 and x2 <= cx2:
+            continue
     
-    if not segs:
-        continue
+    # 위의 경우를 제외하고는 무조건 보임
+    cnt += 1
+    mx1 = x1
+    mx2 = x2
+    # 지워질 선분들 관리(현재 선분과 병합될 선분들)
+    to_remove = []
+    for i in range(max(0, checkIdx), len(covered)):
+        cx1, cx2 = covered[i]
+        if cx1 > x2:
+            break
+        if not(cx1 > x2 or cx2 < x1):
+            mx1 = min(mx1, cx1)
+            mx2 = max(mx2, cx2)
+            to_remove.append((cx1, cx2))
     
-    while segs and segs[0] in deleted:
-        heapq.heappop(segs)
+    for ele in to_remove:
+        covered.remove(ele)
     
-    if segs and not visible[segs[0][1]]:
-        visible[segs[0][1]] = True
-        cnt += 1
-
+    covered.add((mx1, mx2))
+    
 print(cnt)
