c287f6ba3fcb2cfe4ac537a96ee1f5894e5a56a2
Jeong Mingyo <alsry922@gmail.com>
2026-05-28 15:59:08 +0900

    [Passed] 선분 바라보기 2 (856ms, 45MB)

--- trail5/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ trail5/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -1,25 +1,28 @@
 from sortedcontainers import SortedSet
 n = int(input())
+
 points = []
 for i in range(n):
     yi, x1i, x2i = map(int, input().split())
-    points.append( (x1i, +1, i, yi) )
-    points.append( (x2i, -1, i, yi) )
-visible = [False] * n
+    points.append(( x1i, +1, i, yi ))
+    points.append(( x2i, -1, i, yi ))
 
-points.sort()
 # Please write your code here.
+points.sort()
 segs = SortedSet()
+visible = [False] * n
 cnt = 0
-for _, v, index, y in points:
-    # 시작점인 경우
+for x, v, index, y in points:
     if v == 1:
         segs.add((y, index))
-    # 끝점인 경우
+    
     else:
         segs.remove((y, index))
-
-    if segs and not visible[segs[0][1]]:
+    
+    if not segs:
+        continue
+    
+    if not visible[segs[0][1]]:
         visible[segs[0][1]] = True
         cnt += 1
 
