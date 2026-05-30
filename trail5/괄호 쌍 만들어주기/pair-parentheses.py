6334de0110227387bd34fb1b2f838e677b926805
Jeong Mingyo <alsry922@gmail.com>
2026-05-30 17:47:28 +0900

    [Passed] 괄호 쌍 만들어주기 (75ms, 18MB)

--- trail5/괄호 쌍 만들어주기/pair-parentheses.py
+++ trail5/괄호 쌍 만들어주기/pair-parentheses.py
@@ -4,7 +4,7 @@ A = input()
 
 two_close = [0] * len(A)
 
-for i in range(len(A) - 2, 0, -1):
+for i in range(len(A) - 2, -1, -1):
     two_close[i] = two_close[i + 1]
     if A[i] == ')' and A[i + 1] == ')':
         two_close[i] += 1
