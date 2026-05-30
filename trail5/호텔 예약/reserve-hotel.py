6e8a3cee4c5d5c74822ca224fb92c3af0e64d3cf
Jeong Mingyo <alsry922@gmail.com>
2026-05-30 19:10:55 +0900

    [Passed] 호텔 예약 (931ms, 52MB)

--- trail5/호텔 예약/reserve-hotel.py
+++ trail5/호텔 예약/reserve-hotel.py
@@ -1,22 +1,20 @@
 n = int(input())
 intervals = [tuple(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
 points = []
 
+# Please write your code here.
 for i in range(n):
     si, ei = intervals[i]
-    points.append((si, -1, i))
-    points.append((ei, +1, i))
-
+    points.append((si - 1, +1, i))
+    points.append((ei, -1, i))
 points.sort()
 max_occupied = 0
 occupied = set()
-for x, v, i in points:
-    if v == -1:
-        occupied.add(i)
+for x, v, index in points:
+    if v == 1:
+        occupied.add(index)
+        max_occupied = max(max_occupied, len(occupied))
     else:
-        occupied.remove(i)
-    max_occupied = max(max_occupied, len(occupied))
+        occupied.remove(index)
 
 print(max_occupied)
