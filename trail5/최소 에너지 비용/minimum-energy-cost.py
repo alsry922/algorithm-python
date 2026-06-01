f8d00666bd8cc91e0a7c0f9f4dd9f208a19f3886
Jeong Mingyo <alsry922@gmail.com>
2026-06-01 12:09:00 +0900

    [Passed] 최소 에너지 비용 (62ms, 18MB)

---
+++ trail5/최소 에너지 비용/minimum-energy-cost.py
@@ -0,0 +1,19 @@
+import sys
+n = int(input())
+dist = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+MAX = sys.maxsize
+# Please write your code here.
+# 각 장소에서 맨 오른쪽 장소까지 이동하는데 드는 에너지
+need_energy = [0] * n
+for i in range(n - 2, -1, -1):
+    need_energy[i] = need_energy[i + 1] + dist[i]
+
+min_so_far = cost[0]
+total = dist[0] * cost[0]
+
+for i in range(1, n - 1):
+    min_so_far = min(min_so_far, cost[i])
+    total += dist[i] * min_so_far
+
+print(total)
