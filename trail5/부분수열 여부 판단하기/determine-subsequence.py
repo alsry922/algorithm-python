3835eb8e6b1087370fc373303944c1d942611787
Jeong Mingyo <alsry922@gmail.com>
2026-06-04 15:55:59 +0900

    [Passed] 부분수열 여부 판단하기 (76ms, 23MB)

--- trail5/부분수열 여부 판단하기/determine-subsequence.py
+++ trail5/부분수열 여부 판단하기/determine-subsequence.py
@@ -1,19 +1,18 @@
 n, m = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-B = [0] + list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
 # Please write your code here.
-
+j = 0
 is_subsequence = True
-i = 1
-for j in range(1, m + 1):
-    while i <= n and A[i] != B[j]:
-        i += 1
+for i in range(m):
+    while j < n and A[j] != B[i]:
+        j += 1
     
-    if i == n + 1:
+    if j == n:
         is_subsequence = False
         break
-
+    
 if is_subsequence:
     print('Yes')
 else:
