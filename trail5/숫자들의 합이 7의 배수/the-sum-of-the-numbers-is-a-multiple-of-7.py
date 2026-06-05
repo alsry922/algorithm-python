925a8b42cc89a8802d5f9ff875a33ab187be6bf2
Jeong Mingyo <alsry922@gmail.com>
2026-06-05 15:44:29 +0900

    [Passed] 숫자들의 합이 7의 배수 (49ms, 16MB)

--- trail5/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ trail5/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -3,22 +3,25 @@ numbers = [0] + [int(input()) for _ in range(n)]
 
 # Please write your code here.
 
-remainders = [0] * (n + 1)
-min_idx = [-1] * 7
-sum_val = 0
+MOD = 7
+max_idx = [-1] * MOD # 특정 나머지가 등장한 인덱스 중 최대 인덱스
+min_idx = [n + 1] * MOD # 특정 나머지가 등장한 인덱스 중 최소 인덱스
+min_idx[0] = max_idx[0] = 0
+sum_div7 = 0
 for i in range(1, n + 1):
-    sum_val += numbers[i]
-    remainders[i] = sum_val % 7
+    sum_div7 += numbers[i]
+    sum_div7 %= MOD
+    
+    max_idx[sum_div7] = max(max_idx[sum_div7], i)
+    min_idx[sum_div7] = min(min_idx[sum_div7], i)
 
 # print(numbers)
 # print(remainders)
 # print(min_idx)
 
 answer = 0
-for i in range(n + 1):
-    if min_idx[remainders[i]] == -1:
-        min_idx[remainders[i]] = i
-    answer = max(answer, i - min_idx[remainders[i]]
-    )
+
+for i in range(MOD):
+    answer = max(answer, max_idx[i] - min_idx[i])
 
 print(answer)
