27488d7c13de7810a37262a5f33d4e956762d10e
Jeong Mingyo <alsry922@gmail.com>
2026-06-07 00:03:01 +0900

    [Passed] 가장 짧은 부분합 (61ms, 19MB)

--- trail5/가장 짧은 부분합/shortest-subtotal.py
+++ trail5/가장 짧은 부분합/shortest-subtotal.py
@@ -1,21 +1,21 @@
 n, s = map(int, input().split())
-arr = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
 # Please write your code here.
 
 MAX_LENGTH = 100000
 sum_val = 0 # 지금까지의 합
-j = 0 # 다음 뽑을 인덱스
+j = 0 
 answer = MAX_LENGTH
-for i in range(n):
-    while j < n and sum_val < s:
-        sum_val += arr[j]
+for i in range(1, n + 1):
+    while j + 1 <= n and sum_val < s:
+        sum_val += arr[j + 1]
         j += 1
-    
+
     if sum_val < s:
         break
-    
-    answer = min(answer, j - i)
+
+    answer = min(answer, j - i + 1)
 
     sum_val -= arr[i]
 
