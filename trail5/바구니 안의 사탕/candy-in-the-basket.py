37cfff15bcc89d04e90ce17e04ccb33855be2f63
Jeong Mingyo <alsry922@gmail.com>
2026-06-06 14:00:38 +0900

    [Passed] 바구니 안의 사탕 (478ms, 25MB)

--- trail5/바구니 안의 사탕/candy-in-the-basket.py
+++ trail5/바구니 안의 사탕/candy-in-the-basket.py
@@ -1,21 +1,22 @@
 n, k = map(int, input().split())
-MAX_POS = 1000000
-candy = [0] * (MAX_POS + 1) # x 위치에 v개의 사탕 갯수 관리
+candy = []
 for _ in range(n):
     c, p = map(int, input().split())
-    candy[p] += c
+    candy.append((p, c))
 
-# Please write your code here.
-psum = [0] * (MAX_POS + 1)
-psum[0] = candy[0]
-
-for i in range(1, MAX_POS + 1):
-    psum[i] = psum[i - 1] + candy[i]
-
+candy.sort()
 answer = 0
-for i in range(MAX_POS + 1):
-    ei = i + (2 * k)
-    if ei > MAX_POS:
-        ei = MAX_POS
-    answer = max(answer, psum[ei] - psum[i] + candy[i])
+j = 0
+# print(candy)
+candy_sum = 0
+for i in range(n):
+    while j < n and candy[j][0] - candy[i][0] <= 2 * k:
+        # print(f'candy[j]={candy[j]}, candy[i]={candy[i]}')
+        candy_sum += candy[j][1]
+        j += 1
+    
+    answer = max(answer, candy_sum)
+
+    candy_sum -= candy[i][1]
+
 print(answer)
