4bd39a99547dfe6f56fa1c6610b266c09cdf5f0c
Jeong Mingyo <alsry922@gmail.com>
2026-06-06 23:48:21 +0900

    [Passed] 정수 두 개의 합 2 (124ms, 20MB)

--- trail5/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ trail5/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -1,17 +1,15 @@
-from bisect import bisect_left, bisect_right
 n, k = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
+arr = [0] + [int(input()) for _ in range(n)]
 
 # Please write your code here.
 arr.sort()
-i, j = 0, n-1
+j = n
 count = 0
-
-for i in range(n):
-    while j >= 0 and arr[i] + arr[j] > k:
+for i in range(1, n + 1):
+    while j > i and arr[i] + arr[j] > k:
         j -= 1
-    
-    if j < 0 or i >= j:
+
+    if i >= j:
         break
     
     count += (j - i)
