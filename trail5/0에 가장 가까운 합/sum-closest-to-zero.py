459f27524cee3ffad559be8893aace76bc1c9aba
Jeong Mingyo <alsry922@gmail.com>
2026-06-09 15:10:27 +0900

    [Passed] 0에 가장 가까운 합 (74ms, 20MB)

--- trail5/0에 가장 가까운 합/sum-closest-to-zero.py
+++ trail5/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -1,18 +1,20 @@
-import sys
 n = int(input())
 a = list(map(int, input().split()))
 
 # Please write your code here.
+# 정렬
 a.sort()
+# i는 증가, j는 감소시켜야 함.
 j = n - 1
-answer = sys.maxsize
-
+answer = float('inf')
 for i in range(n):
     while i < j and a[i] + a[j] > 0:
         answer = min(answer, abs(a[i] + a[j]))
         j -= 1
+
     if i >= j:
         break
-    answer = min(answer, abs(a[i] + a[j]))
     
+    answer = min(answer, abs(a[i] + a[j]))
+
 print(answer)
