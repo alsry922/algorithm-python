436bc1a9516b11d184481910dea4c210dce0dce4
Jeong Mingyo <alsry922@gmail.com>
2026-06-10 08:35:55 +0900

    [Passed] 최소 에너지 비용 (64ms, 18MB)

--- trail5/최소 에너지 비용/minimum-energy-cost.py
+++ trail5/최소 에너지 비용/minimum-energy-cost.py
@@ -1,16 +1,17 @@
 n = int(input())
-dist = [0] + list(map(int, input().split()))
-charge = list(map(int, input().split()))
+dist = [0] + list(map(int, input().split())) # 장소와 장소를 이동할 때 소모하는 에너지
+cost = list(map(int, input().split())) # 각 장소마다 에너지 1을 채우는데 필요한 비용(에너지 충전 비용)
 
 # Please write your code here.
-
-# 다음 위치로 이동할 때 드는 비용을 계산 
-# 다음 위치로 이동할 때 현재 위치의 충전 비용과, 다음 위치의 충전 비용을 비교함.
-# 다음 위치의 충전 비용이 현재 충전 비용보다 싸면 최소 충전 비용 갱신
-answer = 0
-min_charge = charge[0]
+# greedy
+min_cost = [0] * n # 각 장소에서의 최소 에너지 충전 비용
+min_cost[0] = cost[0]
 for i in range(1, n):
-    answer += (dist[i] * min_charge)
-    min_charge = min(min_charge, charge[i])
+    min_cost[i] = min(min_cost[i - 1], cost[i])
+
+# i -> i + 1로 이동할 때 소모 에너지와 최소 에너지 충전 비용을 계산해서 합한다.
+answer = 0
+for i in range(1, n):
+    answer += min_cost[i - 1] * dist[i]
 
 print(answer)
