0513f3b27da3ff2215344c1eeef64d5c2f95f9c5
Jeong Mingyo <alsry922@gmail.com>
2026-06-12 13:44:48 +0900

    [Passed] 연속하는 정수 N개의 합 (73ms, 23MB)

--- trail5/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.py
+++ trail5/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.py
@@ -1,19 +1,25 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-# Please write your code here.
+ans = 0
 
+# 구간을 잡아봅니다.
+sum_val = 0
 j = 0
-answer = 0
-sum_val = arr[0]
-for i in range(n):
-    while j + 1 < n and sum_val + arr[j + 1] <= m:
+for i in range(1, n + 1):
+    # 구간 내 합이 m보다 작으면 계속 진행합니다.
+    while j + 1 <= n and sum_val < m:
         sum_val += arr[j + 1]
         j += 1
-    
+
+    # 구간 내 합이 m이 되면 정답을 1 증가시킵니다.
     if sum_val == m:
-        answer += 1
-    
+        ans += 1
+
+    # 다음 구간으로 넘어가기 전에
+    # arr[i]에 해당하는 값은 구간에서 제외시킵니다.
     sum_val -= arr[i]
 
-print(answer)
+# 정답을 출력합니다.
+print(ans)
