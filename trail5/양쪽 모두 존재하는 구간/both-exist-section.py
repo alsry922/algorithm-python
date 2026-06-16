e7a092852aa9f99e8359c744f40831b02e508cf1
Jeong Mingyo <alsry922@gmail.com>
2026-06-16 16:23:05 +0900

    [Passed] 양쪽 모두 존재하는 구간 (53ms, 16MB)

--- trail5/양쪽 모두 존재하는 구간/both-exist-section.py
+++ trail5/양쪽 모두 존재하는 구간/both-exist-section.py
@@ -1,78 +1,75 @@
-from collections import defaultdict
 n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-outside_cnt = defaultdict(int)
-for i in range(1, n + 1):
-    num = arr[i]
-    outside_cnt[num] += 1
-inside_cnt = defaultdict(int)
-inside_full = 0
-outside_full = len(set(arr)) - 1
-# 특정 구간에 1이상 M이하의 수가 적어도 하나씩 존재할 때 가장 짧은 구간의 길이를 출력
-# 구간 -> 슬라이딩 윈도우
-# 고정된 구간을 이동시키며 구하지 않음 -> 가변 윈도우
-# 슬라이딩 윈도우 문제니까 투 포인터 사용 가능 한가?
-# i, j 투 포인터를 잡음
-#   [i, j] 구간에서 i가 증가하게 되면 1 ~ M 수 중 하나가 줄어들게 됨.
-#   i를 증가시킨 경우에도 조건이 만족한다면, j를 그대로 두거나
-#   만족하지 않는다면, j를 증가시켜서 1 ~ M 사이의 원소 하나를 찾아야 함.
-#   i가 증가할 때 j도 같은 방향으로 움직여야함 -> 단조성 발견, 투 포인터 사용 가능
-# 모든 원소가 몇 번 등장했는지 hashmap(outside)으로 관리
-# 특정 구간에서 어떤 원소가 몇 번 등장했는지 hashmap(inside)으로 관리
-# inside에 원소 추가할 때마다 all에서는 해당 원소의 갯수를 줄임.
-# inside에 원소 제거할 때마다 all에서는 해당 원소의 갯수를 올림.
-# inside에 1 ~ M 이하의 수가 적어도 하나씩 존재할 때까지 반복.
-#   조건을 만족하게 되면 while문 탈출
-#   all에도 1 ~ M 이하의 수가 적어도 하나씩 있는지 확인
-#       있다면 j - i + 1을 계산하여 답 갱신
-#       없다면 답 갱신 X
-#       i값 증가
-#       다시 조건을 만족시키는 경우가 나올 때까지 j를 확장
+# 1이상 M이하의 숫자가 적어도 하나씩 구간 안에 존재하고,
+# 구간 밖에도 존재하는 가장 짧은 구간의 길이를 출력
+# 구간을 잡아서 문제 풀이 -> 슬라이딩 윈도우
+# 가장 짧은 구간 찾기 -> 가변 윈도우
+# 투 포인터 사용 가능?
+#   i, j 포인터 잡았을 때,
+#   i를 증가시키면 1 ~ M 이하의 숫자 중 하나가 없어짐.
+#   이로 인해 조건을 만족하지 않게 되면 j 또한 증가시켜,
+#   조건을 만족시킬 수 있도록 수를 탐색해야함.
+#   따라서, i, j 모두 한 방향으로만 움직임. -> 단조성 확인
 
-# inside_cnt에 1 ~ M 사이의 숫자가 적어도 하나씩 있는지 확인
-def increase_inside_full():
-    global inside_full
-    inside_full += 1
+# 배열 내의 모든 수 등장 횟수를 count하여 저장.(out_count dict)
+# 구간 안에서 1 ~ M 이하의 등장 횐수를 count하여 저장.(in_count dict)
+# i ~ M 이하의 숫자 중 몇 가지의 숫자가 있는지 변수로 저장.(out_condition, in_condition)
+# 이 변수는 특정 숫자가 0이 되거나, 0 -> 1로 증가하는 경우 +- 1 연산을 해서 계산함.
 
+# j + 1 위치의 수를 뽑을 수 있는지 반복 확인
+#   j + 1 위치가 범위를 벗어나면 암됨
+#   j + 1 위치의 수가 in_count 안에 이미 있는 경우
+#       뽑을 수 있음
+#   j + 1 위치의 수가 in_count 안에 없거나 0인 경우
+#       동시에, in_condition이 이미 M인 경우
+#       뽑을 수 없음.
+#   뽑을 수 없는 경우 반복 종료
+
+# out_condition 도 만족하는지 확인 후
+# 만족하면 j - i + 1로 길이 계산 후 정답 갱신
+def push(idx):
+    global in_condition, out_condition
+    num = arr[idx]
+    if in_count[num] == 0:
+        in_condition += 1
+    in_count[num] += 1
+
+    out_count[num] -= 1
+    if out_count[num] == 0:
+        out_condition -= 1
+
+def pop(idx):
+    global in_condition, out_condition
+    num = arr[idx]
+    in_count[num] -= 1
+    if in_count[num] == 0:
+        in_condition -= 1
+    
+    if out_count[num] == 0:
+        out_condition += 1
+    out_count[num] += 1
+
+out_count = [0] * (m + 1)
+in_count = [0] * (m + 1)
+for ele in arr[1:]:
+    out_count[ele] += 1
+out_condition = m
+in_condition = 0
 j = 0
 answer = float('inf')
 for i in range(1, n + 1):
-    # inside 조건을 만족할 때까지 반복
-    while j + 1 <= n and inside_full < m:
-        num = arr[j + 1]
-        # inside에 없던 수가 추가되면, inside_full 증가
-        if inside_cnt[num] == 0:
-            inside_full += 1
-        inside_cnt[num] += 1
-        # inside에 num을 추가하니까 outside에 num을 하나 제거
-        # outside에 num의 갯수가 0이 되면 outside_full 감소
-        if outside_cnt[num] > 0:
-            outside_cnt[num] -= 1
-            if outside_cnt[num] == 0:
-                outside_full -= 1
-            
+    while j + 1 <= n and in_condition < m:
+        push(j + 1)
         j += 1
-    
-    if inside_full < m:
+
+    if in_condition < m:
         break
     
-    # ouside 조건도 만족하면 
-    if outside_full >= m:
-        answer = min(answer, j - i + 1)        
+    if out_condition == m:
+        answer = min(answer, j - i + 1)
     
-    # inside에서 arr[i] 제거
-    # inside에 해당 수가 0이되면 inside_full 감소
-    if inside_cnt[arr[i]] > 0:
-        inside_cnt[arr[i]] -= 1
-        if inside_cnt[arr[i]] == 0:
-            inside_full -= 1
-    # outside에 arr[i] 추가
-    # outside에 arr[i]가 없었으면 outside_full 증가
-    if outside_cnt[arr[i]] == 0:
-        outside_full += 1
-    outside_cnt[arr[i]] += 1
+    pop(i)
 
 print(-1 if answer == float('inf') else answer)
-    
