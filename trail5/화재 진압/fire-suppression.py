565526535aebc7b3e6e896236b217d7c3dddc7d9
Jeong Mingyo <alsry922@gmail.com>
2026-06-17 13:24:24 +0900

    [Passed] 화재 진압 (156ms, 33MB)

--- trail5/화재 진압/fire-suppression.py
+++ trail5/화재 진압/fire-suppression.py
@@ -1,25 +1,24 @@
-import bisect
-
 n, m = map(int, input().split())
 fires = list(map(int, input().split()))
 stations = list(map(int, input().split()))
 
 # Please write your code here.
-stations.sort()
+# fires, stations를 정렬
+# i, j 투 포인터를 잡음
+# i를 증가시킬 때 j를 감소시키면 거리가 더 멀이지기 때문에 j를 유지 혹은 증가시켜야함.
+# 화재 발생 위치 i에서 stations[j] 까지의 거리보다
+#   stations[j + 1] 까지의 거리가 더 짧으면 j를 증가시킨다.
+def dist(i, j):
+    return abs(fires[i] - stations[j])
+
+fires = [0] + sorted(fires)
+stations = [0] + sorted(stations)
+j = 1
 answer = 0
-candidates = []
-for fire in fires:
-    # 불이 날 수 있는 위치와 가장 가까운 소방서 찾기
-    lb = bisect.bisect_left(stations, fire)
-    candidates = []
-    # 큰 게 없는 경우
-    if lb < len(stations):
-        candidates.append(abs(stations[lb] - fire))
-    # 크거나 같은 게 있는경우
-    if lb > 0:
-        candidates.append(abs(stations[lb - 1] - fire))
+for i in range(1, n + 1):
+    while j + 1 <= m and dist(i, j) > dist(i, j + 1):
+        j += 1
     
-    # 이 fire에서 가장 가까운 소방서까지의 거리
-    answer = max(answer, min(candidates))
-    
+    answer = max(answer, dist(i, j))
+
 print(answer)
