22891bd8845040277367be0c38a863012a16f6c8
Jeong Mingyo <alsry922@gmail.com>
2026-06-19 12:09:40 +0900

    [Passed] 숫자 빠르게 찾기 (175ms, 24MB)

--- trail5/숫자 빠르게 찾기/find-number-fast.py
+++ trail5/숫자 빠르게 찾기/find-number-fast.py
@@ -4,15 +4,18 @@ queries = [int(input()) for _ in range(m)]
 
 # Please write your code here.
 for query in queries:
-    left, right = 0, n - 1
-    pos = -1
-    while left <= right:
-        mid = (left + right) // 2
+    l, r = 0, n - 1
+    pos = n
+    while l <= r:
+        mid = (l + r) // 2
         if arr[mid] == query:
-            pos = mid + 1
-            break
-        if query > arr[mid]:
-            left = mid + 1
+           pos = mid
+           break
+        if query < arr[mid]:
+            r = mid - 1
         else:
-            right = mid - 1
-    print(pos)
+            l = mid + 1
+    if pos == n:
+        print(-1)
+    else:
+        print(pos + 1)
