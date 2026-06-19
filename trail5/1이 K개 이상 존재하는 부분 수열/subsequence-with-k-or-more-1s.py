d975f8869ac4b48aee225ae5cdb22a71a29977d0
Jeong Mingyo <alsry922@gmail.com>
2026-06-19 11:17:01 +0900

    [Passed] 1이 K개 이상 존재하는 부분 수열 (319ms, 114MB)

--- trail5/1이 K개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1s.py
+++ trail5/1이 K개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1s.py
@@ -2,34 +2,32 @@ n, k = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-# 조건에 맞는 연속 부분 수열 구하기
-# 연속 부분 수열 -> 슬라이딩 윈도우
-# 가장 짧은 구간 찾기 -> 가변 윈도우
-# 투 포인터 i, j 사용 가능?
-#   i를 왼쪽 j를 오른쪽 포인터로 설정했을 때, i가 증가하면 1이 하나 줄어들 수 있으니까
-#   조건을 만족시키기 위해서 j가 뒤로갈 필요가 없음
-#   단조성 확인되므로 투 포인터 사용 가능
-
-# while문을 반복하며 다음 위치(j + 1)가 범위 안에 존재하며,
-# 지금까지 뽑은 1의 갯수가 k 미만인 경우 반복 가능
-# while loop 탈출하고 길이 계산 후 답을 갱신
-# 다음 loop 진입 전에 i번째 수가 1인 경우 1 count 변수 감소시켜야 함.
-# 그리고 i를 증가시킴
-MIN = float('inf')
-answer = MIN
+# 가장 짧은 연속 부분 수열 -> 가변 슬라이딩 윈도우
+# 투 포인터 i, j 사용 가능 여부
+#   i가 증가할 때 1이 줄어들 수 있음.
+#   K개 이상 존재하기 위해서는 j를 뒤로 돌릴 수 없음.
+#   j 또한 증가시켜서 1을 찾아야 함.
+#   단조성 확인을 사용 가능
+# while문을 반복하며 j를 증가
+#   j + 1이 범위 안에 있고, 지금까지 고른 1의 갯수가 K개 미만이면
+#   j + 1 위치의 수를 뽑을 수 있음.
+#   뽑은 수가 1이라면 1의 갯수를 증가시키고
+#   j도 증가시킴
+# while loop 탈출
+#   조건이 맞지 않아 탈출하게 되는 경우
+#   j - i + 1로 길이를 계산하여 답을 갱신함.
 j = 0
 count = 0
+MIN = float('inf')
+answer = MIN
 for i in range(1, n + 1):
     while j + 1 <= n and count < k:
         if arr[j + 1] == 1:
             count += 1
         j += 1
-
     if count < k:
         break
-
     answer = min(answer, j - i + 1)
-
     if arr[i] == 1:
         count -= 1
 
