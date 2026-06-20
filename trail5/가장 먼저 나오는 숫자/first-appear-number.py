b24404d1748db077ef56bd8c2a3ae0642416824c
Jeong Mingyo <alsry922@gmail.com>
2026-06-20 23:55:01 +0900

    [Passed] 가장 먼저 나오는 숫자 (155ms, 33MB)

--- trail5/가장 먼저 나오는 숫자/first-appear-number.py
+++ trail5/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,36 +1,37 @@
 n, m = map(int, input().split())
-arr = [0] + list(map(int, input().split()))
+arr = list(map(int, input().split()))
 query = list(map(int, input().split()))
 
 # Please write your code here.
-def lower_bound(target, left, right):
-    min_idx = n + 1
+def lower_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
     while left <= right:
         mid = (left + right) // 2
-        if x <= arr[mid]:
+        # if target == arr[mid]:
+        #     min_idx = min(min_idx, mid)
+        if target <= arr[mid]:
             min_idx = min(min_idx, mid)
             right = mid - 1
         else:
             left = mid + 1
     return min_idx
 
-def upper_bound(target, left, right):
-    min_idx = n + 1
+def upper_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
     while left <= right:
         mid = (left + right) // 2
-        if x < arr[mid]:
+        # if target == arr[mid]:
+        #     min_idx = min(min_idx, mid)
+        if target < arr[mid]:
             min_idx = min(min_idx, mid)
             right = mid - 1
         else:
             left = mid + 1
     return min_idx
 
-for x in query:
-    left, right = 1, n
-    min_idx = n + 1
-    lb = lower_bound(x, left, right)
-    ub = upper_bound(x, left, right)
-    if lb == ub:
-        print(-1)
-    else:
-        print(lb)
+for target in query:
+    lb = lower_bound(target)
+    ub = upper_bound(target)
+    print(-1 if lb == ub else lb + 1)
