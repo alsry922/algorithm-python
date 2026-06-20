900e4877e1c805135524e236329491869c3b8cb2
Jeong Mingyo <alsry922@gmail.com>
2026-06-21 00:03:30 +0900

    [Passed] 겹치는 원소가 적은 부분 수열 (118ms, 30MB)

--- trail5/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.py
+++ trail5/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.py
@@ -3,28 +3,23 @@ n, k = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-# 투 포인터 사용 가능
-# i, j 투 포인터 i 가 증가했을 때 뽑은 원소 하나가 사라지는 것이기 때문에
-# j를 증가시켜 새로운 원소를 뽑아 길이를 더 길게 만들 수 있음
-# j + 1(다음 수)가 범위 안에 있고, 현재 k개 미만인 경우 뽑을 수 있음.
-# 반복하며 j를 증가시키다가 조건을 만족시키지 못하는 경우, loop 탈출
-# j가 끝까지 이동했으면(모든 수를 다 살펴본 경우) outer loop break
-#   이 구간이 답이므로
-# j가 아직 남은 경우에 조건이 만족된 경우는 answer를 갱신
+# 같은 값이 k개 이하가 되도록 배열에서 원소를 연속으로 뽑았을 때 그 길이가 최대가 되도록 하라.
+# 각 값이 현재 몇 개가 뽑혔는지 dict로 관리
+# i, j를 왼쪽, 오른쪽 포인터로 두고 투 포인터 탐색
+# while 을 반복하며 j + 1이 범위 안에 있고, j + 1 위치의 수가 현재 k개 미만이면 j를 확장 가능
+#   안되면 j 확장을 멈추고 현재까지의 연속 부분 수열의 길이를 구해서 답을 갱신
+#   i를 확장하면서 i 위치의 수의 count를 하나 줄임.
 
 count = defaultdict(int)
-answer = 1
 j = 0
+answer = 1
 for i in range(1, n + 1):
     while j + 1 <= n and count[arr[j + 1]] < k:
         count[arr[j + 1]] += 1
         j += 1
-    
-    answer = max(answer, j - i + 1)
 
-    if j == n:
-        break
-    
+    answer = max(answer, j - i + 1) 
+
     count[arr[i]] -= 1
 
 print(answer)
