f5454637a66f5965854c2a41bb6cfb00b2e6acab
Jeong Mingyo <alsry922@gmail.com>
2026-06-23 23:24:20 +0900

    [Passed] 최대 거리의 점 (453ms, 22MB)

--- trail5/최대 거리의 점/maximum-distance-point.py
+++ trail5/최대 거리의 점/maximum-distance-point.py
@@ -3,36 +3,28 @@ n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-
-# 가장 인접한 두 물건의 거리 d(후보값)
-# d가 증가하면 배치할 수 있는 물건의 갯수가 적어짐
-# 단조성 확인
+# 후보값: 두 물건 사이의 거리 d
+# 결정함수: 최소 거리를 d라고 가정했을 때 M개 이상의 물건을 설치할 수 있는 가?
 arr.sort()
-# print(arr)
-def can_place(x):
+def is_possible(d):
     last_pos = arr[0]
     cnt = 1
-    for i in range(1, n):
-        if arr[i] - last_pos >= x:
-            cnt += 1
-            last_pos = arr[i]
-    # while bisect.bisect_left(arr, last_pos + x) < len(arr):
-    #     lb = bisect.bisect_left(arr, last_pos + x)
-    #     last_pos = arr[lb]
-    #     cnt += 1
-    
-    return cnt >= m
+    while cnt < m:
+        lb = bisect.bisect_left(arr, last_pos + d)
+        if lb == n:
+            return False
+        cnt += 1
+        last_pos = max(arr[lb], last_pos + d)
+    return True
 
-
-left, right = 0, arr[n - 1] - arr[0]
-answer = 0
-while left <= right:
-    mid = (left + right) // 2
-    # print(left, mid, right)
-    if can_place(mid):
+lo, hi = 0, 10 ** 9
+answer = 1
+while lo <= hi:
+    mid = (lo + hi) // 2
+    if is_possible(mid):
         answer = max(answer, mid)
-        left = mid + 1
+        lo = mid + 1
     else:
-        right = mid - 1
+        hi = mid - 1
 
 print(answer)
