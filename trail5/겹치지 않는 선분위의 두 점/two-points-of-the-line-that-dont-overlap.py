0feb74dc1b5ba6290d40f10fd2d9beb1cccf8c97
Jeong Mingyo <alsry922@gmail.com>
2026-06-24 14:57:34 +0900

    [Passed] 겹치지 않는 선분위의 두 점 (1026ms, 28MB)

--- trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -1,30 +1,28 @@
-import bisect
 n, m = map(int, input().split())
 segments = [tuple(map(int, input().split())) for _ in range(m)]
 segments.sort()
-x1s = [x1 for x1, x2 in segments]
-x2s = [x2 for x1, x2 in segments]
-
 # Please write your code here.
-# 후보값 d: 가장 가까운 두 점 거리의 최댓값
-# 후보값 d를 가정했을 떄 조건을 만족하는가?
-# 최소 거리가 d 이상이 되도록 N개의 점을 배치할 수 있는가?
+# 후보값 d: 두 점 사이의 최소 거리
+# d를 특정 값으로 가정했을 때 n개 이상의 점을 선분 위에 배치 가능한가?
+# 최소 거리를 d로 가정했을 때 n개 이상의 점을 배치 가능하다면, d를 증가시켜볼 수 있음
+# 반대라면 d를 감소시켜야 함.
 
 def is_possible(d):
+    last_pos = segments[0][0]
     cnt = 1
-    last_pos = x1s[0]
-    while cnt < n:
-        lb = bisect.bisect_left(x2s, last_pos + d)
-        if lb == m:
-            return False
-        last_pos = max(segments[lb][0], last_pos + d)
-        cnt += 1
-    return True
+    for i in range(m):
+        x1, x2 = segments[i]
+        while last_pos + d <= x2:
+            last_pos = max(x1, last_pos + d)
+            cnt += 1
+    
+    return cnt >= n
 
-left, right = 0, 10 ** 18 + 1
+left, right = 0, 10 ** 18 // (n - 1)
 answer = 0
 while left <= right:
     mid = (left + right) // 2
+    # print(left, mid, right)
     if is_possible(mid):
         answer = max(answer, mid)
         left = mid + 1
