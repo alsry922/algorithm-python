2dab58075664998db92b74d445a955f2173bf85d
Jeong Mingyo <alsry922@gmail.com>
2026-06-25 15:06:19 +0900

    [Passed] 수의 개수 (183ms, 23MB)

--- trail5/수의 개수/number-of-integers.py
+++ trail5/수의 개수/number-of-integers.py
@@ -4,30 +4,30 @@ queries = [int(input()) for _ in range(m)]
 
 # Please write your code here.
 def lower_bound(target):
-    l, r = 0, n - 1
+    left, right = 0, n - 1
     min_idx = n
-    while l <= r:
-        mid = (l + r) // 2
+    while left <= right:
+        mid = (left + right) // 2
         if target <= arr[mid]:
             min_idx = min(min_idx, mid)
-            r = mid - 1
+            right = mid - 1
         else:
-            l = mid + 1
+            left = mid + 1
     return min_idx
 
 def upper_bound(target):
-    l, r = 0, n - 1
+    left, right = 0, n - 1
     min_idx = n
-    while l <= r:
-        mid = (l + r) // 2
+    while left <= right:
+        mid = (left + right) // 2
         if target < arr[mid]:
             min_idx = min(min_idx, mid)
-            r = mid - 1
+            right = mid - 1
         else:
-            l = mid + 1
+            left = mid + 1
     return min_idx
 
 for query in queries:
     lb = lower_bound(query)
-    rb = upper_bound(query)
-    print(rb - lb)
+    ub = upper_bound(query)
+    print(ub - lb)
