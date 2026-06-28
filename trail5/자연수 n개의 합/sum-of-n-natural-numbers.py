1cb7b523d1d730afc2fac46bb70a02fd0089fe13
Jeong Mingyo <alsry922@gmail.com>
2026-06-28 10:56:30 +0900

    [Passed] 자연수 n개의 합 (48ms, 16MB)

--- trail5/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ trail5/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -1,22 +1,14 @@
 s = int(input())
 
 # Please write your code here.
-# 후보값 x([1, x]) 가 커질수록 합도 커짐.
-# 후보값 x가 작아질수록 합도 작아짐.
-# 단조성 확인
-
-# n(n + 1) <= 2s
-# n^2 <= 2s
-# n^2 <= 2 * 10^18
-# n <= 2 * 10 ^ 9
-
-left, right = 1, 2 * 10 ** 9
+left, right = 1, 10 ** 18
 answer = 0
 while left <= right:
     mid = (left + right) // 2
-    if mid * (mid + 1) // 2 <= s:
-        answer = max(answer, mid)
+    sum_val = mid * (mid + 1) // 2
+    if sum_val <= s:
         left = mid + 1
+        answer = max(answer, mid)
     else:
         right = mid - 1
 
