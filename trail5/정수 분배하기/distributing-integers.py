9756e41a0bf8533a940f92daf6e8f1cad3fe9925
Jeong Mingyo <alsry922@gmail.com>
2026-06-28 11:06:52 +0900

    [Passed] 정수 분배하기 (67ms, 16MB)

--- trail5/정수 분배하기/distributing-integers.py
+++ trail5/정수 분배하기/distributing-integers.py
@@ -2,23 +2,21 @@ n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-# 후보값 x = 분배할 정수
-# x 가 줄어들 수록 각 수에서 분배할 수 있는 횟수가 많아짐
-# 단조성 확인
+
 def is_possible(x):
     count = 0
-    for ele in arr:
-        count += ele // x
+    for num in arr:
+        count += num // x
+    
     return count >= m
 
 left, right = 1, 100000
 answer = 0
-
 while left <= right:
     mid = (left + right) // 2
     if is_possible(mid):
-        answer = mid
         left = mid + 1
+        answer = max(answer, mid)
     else:
         right = mid - 1
 
