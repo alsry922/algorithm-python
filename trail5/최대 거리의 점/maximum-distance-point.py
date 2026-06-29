bdb9d0e711af9220f03d32aab39176db3a21e168
Jeong Mingyo <alsry922@gmail.com>
2026-06-29 21:53:40 +0900

    [Passed] 최대 거리의 점 (223ms, 21MB)

--- trail5/최대 거리의 점/maximum-distance-point.py
+++ trail5/최대 거리의 점/maximum-distance-point.py
@@ -1,28 +1,44 @@
-import bisect
 n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
-
-# Please write your code here.
-# 후보값: 두 물건 사이의 거리 d
-# 결정함수: 최소 거리를 d라고 가정했을 때 M개 이상의 물건을 설치할 수 있는 가?
 arr.sort()
+# Please write your code here.
+
+# 가장 인접한 두 물건의 거리를 d라고 했을 때 d의 최댓값 출력
+# d를 1부터 10 ** 9 까지 다 해볼 수 없음
+# d가 커질수록 설치할 수 있는 물건의 갯수가 적어짐, 반대는 많아짐 -> 단조성
+# 결정함수:
+#   d를 가정했을 때 물건 m개를 다 설치할 수 있는가?
+
+# 수도코드
+# def is_possible(d):
+# 첫 시작을 arr[0]으로 함
+# 물건 갯수(cnt)는 1로 시작
+# arr를 1인덱스부터 순회하면서 시작 위치 + d의 값 이상인 원소를 찾아 점을 배치하고 물건 갯수를 하나 늘림.
+# 배치한 점을 시작위치로 지정하여 시뮬레이션을 계속함.
+# 시뮬레이션이 끝난 후 cnt >= m 인지 판단.
+
+# 시간복잡도
+# 결정함수 O(N)
+# 최종 시간복잡도 -> O(N * log 10^9)
+
 def is_possible(d):
-    last_idx = 0
+    last_pos = arr[0]
     cnt = 1
     for i in range(1, n):
-        if arr[i] - arr[last_idx] >= d:
-            last_idx = i
+        if last_pos + d <= arr[i]:
+            last_pos = arr[i]
             cnt += 1
+    
     return cnt >= m
 
-lo, hi = 0, 10 ** 9
+left, right = 1, 10 ** 9
 answer = 1
-while lo <= hi:
-    mid = (lo + hi) // 2
+while left <= right:
+    mid = (left + right) // 2
     if is_possible(mid):
+        left = mid + 1
         answer = max(answer, mid)
-        lo = mid + 1
     else:
-        hi = mid - 1
+        right = mid - 1
 
 print(answer)
