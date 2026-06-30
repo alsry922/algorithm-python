06ccce42507eef4d36155526cd332c8345539cc2
Jeong Mingyo <alsry922@gmail.com>
2026-06-30 13:18:32 +0900

    [Passed] 겹치지 않는 선분위의 두 점 (1421ms, 28MB)

--- trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -1,34 +1,51 @@
 n, m = map(int, input().split())
 segments = [tuple(map(int, input().split())) for _ in range(m)]
-segments.sort()
+
 # Please write your code here.
-# 후보값 d: 두 점 사이의 최소 거리
-# d를 특정 값으로 가정했을 때 n개 이상의 점을 선분 위에 배치 가능한가?
-# 최소 거리를 d로 가정했을 때 n개 이상의 점을 배치 가능하다면, d를 증가시켜볼 수 있음
-# 반대라면 d를 감소시켜야 함.
+segments.sort()
+# 가장 가까운 두 점의 거리 d
+# d의 최댓값을 출력하는 문제
+# 0 <= d <= 10 ** 18 이기 때문에, 모든 d에 대해서 완전탐색은 불가능
+
+# 이분 탐색으로 d의 범위를 좁혀가면서 답을 찾을 수 있나?
+# d가 특정 값 이상이면 N개의 점을 배치할 수 없음.
+# d가 특정 값 미만이면 N개의 점을 배치할 수 있음.
+# 단조성 확인 -> 이분 탐색 가능
+
+# 수도코드
+# segmets.sort()
+# 결정함수(d):
+#   point_count = 0
+#   last_pos = -1
+#   for i in range(m):
+#       start, end = segments[i]
+#       last_pos = max(last_pos, start)
+#       point_count += 1
+#       cnt = (end - last_pos) // d
+#       last_pos = last_pos + cnt * d
+#       point_count += cnt
+#   return point_count >= n
 
 def is_possible(d):
-    last_pos = segments[0][0]
-    cnt = 1
+    point_count = 0
+    last_pos = -1
     for i in range(m):
-        x1, x2 = segments[i]
-        while last_pos + d <= x2:
-            last_pos = max(x1, last_pos + d)
-            cnt += 1
+        start, end = segments[i]
+        last_pos = max(start, last_pos)
+        point_count += 1
+        cnt = (end - last_pos) // d
+        last_pos = last_pos + cnt * d + d
+        point_count += cnt
+        
+    return point_count >= n
 
-            if cnt >= n:
-                break
-    
-    return cnt >= n
-
-left, right = 0, 10 ** 18 // (n - 1)
-answer = 0
+left, right = 1, 10 ** 18
+answer = 1
 while left <= right:
     mid = (left + right) // 2
-    # print(left, mid, right)
     if is_possible(mid):
-        answer = max(answer, mid)
         left = mid + 1
+        answer = max(answer, mid)
     else:
         right = mid - 1
 
