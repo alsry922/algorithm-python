6c2306400c8dbba7146e05b4d3bf069171e5f441
Jeong Mingyo <alsry922@gmail.com>
2026-07-04 12:51:20 +0900

    [Passed] 계단 오르기 (52ms, 16MB)

--- trail4/계단 오르기/climbing-stairs.py
+++ trail4/계단 오르기/climbing-stairs.py
@@ -5,6 +5,6 @@ dp = [0] * (MAX_N + 1)
 dp[2] = 1
 dp[3] = 1
 for i in range(4, MAX_N + 1):
-    dp[i] = (dp[i - 2] + dp[i - 3]) % 10007
+    dp[i] = dp[i - 2] + dp[i - 3]
 
-print(dp[n])
+print(dp[n] % 10007)
