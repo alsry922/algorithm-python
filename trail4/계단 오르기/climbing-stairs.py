adb394e6a8d92874e503e1b1eb9f271ebeddd415
Jeong Mingyo <alsry922@gmail.com>
2026-07-04 13:02:28 +0900

    [Passed] 계단 오르기 (48ms, 16MB)

--- trail4/계단 오르기/climbing-stairs.py
+++ trail4/계단 오르기/climbing-stairs.py
@@ -5,6 +5,9 @@ dp = [0] * (MAX_N + 1)
 dp[2] = 1
 dp[3] = 1
 for i in range(4, MAX_N + 1):
-    dp[i] = dp[i - 2] + dp[i - 3]
+    dp[i] = (dp[i - 2] + dp[i - 3]) % 10007
 
-print(dp[n] % 10007)
+print(dp[n])
+
+# 모듈로 연산은 분배법칙 성립함
+#   (a + b) % m = ((a % m) + (b % m)) % m
