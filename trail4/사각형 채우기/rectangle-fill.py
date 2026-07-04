813fe1f6203d27865b28936685a711570ef41598
Jeong Mingyo <alsry922@gmail.com>
2026-07-04 13:02:50 +0900

    [Passed] 사각형 채우기 (51ms, 16MB)

--- trail4/사각형 채우기/rectangle-fill.py
+++ trail4/사각형 채우기/rectangle-fill.py
@@ -5,6 +5,6 @@ dp = [0] * (MAX_N + 1)
 dp[0] = 1
 dp[1] = 1
 for i in range(2, MAX_N + 1):
-    dp[i] = dp[i- 1] + dp[i - 2]
+    dp[i] = (dp[i- 1] + dp[i - 2]) % 10007
 
-print(dp[n] % 10007)
+print(dp[n])
