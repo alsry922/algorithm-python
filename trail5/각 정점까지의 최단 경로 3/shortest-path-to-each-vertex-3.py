1c6800b1909841b9ecc313dda89f2f7053d57343
Jeong Mingyo <alsry922@gmail.com>
2026-07-21 23:39:28 +0900

    [Passed] 각 정점까지의 최단 경로 3 (82ms, 18MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,32 +1,27 @@
-from collections import deque
 import heapq
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-MAX = float('inF')
+graph = [[] for _ in range(n + 1)] # 인접리스트 그래프 구현
+INF = float('inf')
+dist = [INF for _ in range(n + 1)]
+dist[1] = 0
 # Please write your code here.
-dist = [MAX] * (n + 1)
-graph = [
-    [] for _ in range(n + 1)
-]
-for edge in edges:
-    start, end, value = edge
+for start, end, value in edges:
     graph[start].append((end, value))
 
+def dijkstra(graph):
+    q = []
 
-def dijkstra(graph, source):
-    dist[source] = 0
-    heap = []
-    for v in range(1, n + 1):
-        heapq.heappush(heap, (dist[v], v))
+    for i in range(1, n + 1):
+        heapq.heappush(q, (dist[i], i))
+    
+    while q:
+        min_dist, cv = heapq.heappop(q)
+        for end, value in graph[cv]:
+            if min_dist + value < dist[end]:
+                dist[end] = min_dist + value
+                heapq.heappush(q, (dist[end], end))
+dijkstra(graph)
 
-    while heap:
-        cur_dist, u = heapq.heappop(heap)
-        for end, value in graph[u]:
-            alt = dist[u] + value
-            if alt < dist[end]:
-                dist[end] = alt
-                heapq.heappush(heap, (alt, end))
-
-dijkstra(graph, 1)
 for i in range(2, n + 1):
-    print(-1 if dist[i] == MAX else dist[i])
+    print(-1 if INF == dist[i] else dist[i])
