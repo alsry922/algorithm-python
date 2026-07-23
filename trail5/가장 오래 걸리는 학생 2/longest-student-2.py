37a69b3bd0ffa8e899d0a5d20a7fa7596e48c0f2
Jeong Mingyo <alsry922@gmail.com>
2026-07-23 19:47:21 +0900

    [Passed] 가장 오래 걸리는 학생 2 (423ms, 40MB)

--- trail5/가장 오래 걸리는 학생 2/longest-student-2.py
+++ trail5/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -1,40 +1,39 @@
 import heapq
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-graph = [[] for _ in range(n + 1)]
 INF = float('inf')
 dist = [INF for _ in range(n + 1)]
-# 시작점은 최단거리가 0
 dist[n] = 0
+graph = [[] for _ in range(n + 1)]
 # Please write your code here.
-# N번 노드(학교) 에서 집까지 가는 최단거리를 구하고, 그 중 최댓값을 출력
-# 거리 1마다 1초의 이동시간이 걸림
-# 다익스트라 -> 특정 시작점에서 모든 정점까지의 최단 거리를 구함
-# 단방향 그래프에서 특정 도착점으로부터 모든 정점까지의 거리를 구하려면 모든 간선을 뒤집어주어야 함.
+# 모든 정점으로부터 특정 도착점까지의 최단거리
+# 특정 시작점에서 모든 정점까지의 최단거리를 구하는 알고리즘이 다익스트라
+# 모던 정점에서부터 도착점까지릐 최단거리를 구하는 방법?
+# 각 정점마다 다익스트라 알고리즘으로 시뮬레이션 해도 되지만,
+# 모든 간선을 뒤집고, 도착점을 시작점으로하여 다익스트라 알고리즘을 시뮬레이션 해도 된다.
+# 단방향인 경우에 해당하며, 양방향인 경우에는 모든 간선을 뒤집을 필요가 없다.
 
-# 뒤집어서 그래프 구현
-# N이 크니까 인접리스트로 O(E log V) 다익스트라로 구현해야 함.
+# N의 범위가 크기 때문에 heap을 이용하여 다익스트라 알고리즘을 구현한다.
+# 모든 간선을 뒤집어서 그래프를 인접리스트로 구현한다.
 for start, end, value in edges:
     graph[end].append((start, value))
-
-# 우선순위 큐 만들기
-q = [(dist[i], i) for i in range(1, n + 1)]
-heapq.heapify(q)
-
-# 큐에 원소가 남아있을 때까지 반복
+# 힙에 시작점 넣고 다익스트라 시작
+q = [(dist[n], n)]
 while q:
-    # cur_v와 cur_v까지의 최단 거리
+    # 힙에서 최단거리가 구해진 정점 찾아서 제거
     min_dist, cur_v = heapq.heappop(q)
-    # cur_v의 최소값이 갱신되기 전 stale 값은 무시함
-    # 같은 노드가 여러 번 삽입될 수 있기 때문임.
+    # 같은 정점이 여러 번 heap에 들어갈 수 있음.
+    # 이미 최단거리가 구해졌던 정점은 skip 함
     if min_dist > dist[cur_v]:
         continue
-    # cur_v와 연결된 노드들 탐색
+    # cur_v 정점과 연결된 노드를 탐색
     for end, value in graph[cur_v]:
-        # 계산한 최단거리가 현재 end까지의 최단거리보다 작다면 갱신
+        # 현재 end 정점까지의 거리가, cur_v에서 이어진 거리보다 크다면
+        # 최단 거리를 갱신
         if min_dist + value < dist[end]:
             dist[end] = min_dist + value
-            # 노드 추가
+            # 구해진 최단 거리를 큐에 삽입
             heapq.heappush(q, (dist[end], end))
 
 print(max(dist[1:]))
+            
