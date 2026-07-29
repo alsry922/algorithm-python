d4ea88f4aa264a17ebfddacfa3bcd786916e7524
Jeong Mingyo <alsry922@gmail.com>
2026-07-29 21:58:12 +0900

    [Passed] 가장 가까운 거리의 최댓값 (634ms, 48MB)

--- trail5/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.py
+++ trail5/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.py
@@ -1,52 +1,36 @@
-# 다익스트라 알고리즘
-# 특정 시작점부터 도달가능한 모든 정점까지의 최단 거리를 구해준다.
-# 특정 정점에서부터 A, B, C 정점까지의 최단거리를 구하고 그 중 최댓값을 구해야 한다.
-# N이 최대 100000이기 때문에 이 방법은 안됨
-# 그럼 A, B, C는 정해졌으니 A, B, C 각각에서 시작해서 다익스트라를 구한다.
-# 다익스트라로 distA, distB, distC를 구하고, 
-# distA, distB, distC에서 모두 도달 가능한 정점 중 최댓값을 구하면 될 것 같다.
 import sys, heapq
 input = sys.stdin.readline
 n, m = map(int, input().split())
 a, b, c = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-graph = [[] for _ in range(n + 1)]
 INF = sys.maxsize
+graph = [[] for _ in range(n + 1)]
+# Please write your code here.
 for s, e, v in edges:
     graph[s].append((e, v))
     graph[e].append((s, v))
 
-distA = [INF for _ in range(n + 1)]
-distA[a] = 0
-heapA = [(distA[a], a)]
+dist_abc = [INF for _ in range(n + 1)]
 
-distB = [INF for _ in range(n + 1)]
-distB[b] = 0
-heapB = [(distB[b], b)]
-
-distC = [INF for _ in range(n + 1)]
-distC[c] = 0
-heapC = [(distC[c], c)]
-
-def dijkstra(dist, heap):
+def dijkstra(start):
+    dist = [INF for _ in range(n + 1)]
+    dist[start] = 0
+    heap = [(dist[start], start)]
     while heap:
-        min_dist, cv = heapq.heappop(heap)
-        if min_dist > dist[cv]:
+        min_dist, cur_v = heapq.heappop(heap)
+        if min_dist > dist[cur_v]:
             continue
-        for e, v in graph[cv]:
+        for e, v in graph[cur_v]:
             if min_dist + v < dist[e]:
                 dist[e] = min_dist + v
                 heapq.heappush(heap, (dist[e], e))
-dijkstra(distA, heapA)
-dijkstra(distB, heapB)
-dijkstra(distC, heapC)
 
-answer = 0
-for i in range(1, n + 1):
-    if distA[i] == INF or \
-        distB[i] == INF or \
-        distC[i] == INF:
-        continue
-
-    answer = max(answer, min(distA[i], distB[i], distC[i]))
+    
+    for i in range(1, n + 1):
+        dist_abc[i] = min(dist_abc[i], dist[i])
+    
+dijkstra(a)
+dijkstra(b)
+dijkstra(c)
+answer = max(dist_abc[1:])
 print(answer)
